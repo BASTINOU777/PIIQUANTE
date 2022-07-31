a02c3a50230c7bcedc9087da64eb33b8434a77eb
ABastinou <101596380+BASTINOU777@users.noreply.github.com>
2022-07-31 23:45:03 +0200

    initial commit installation de nodemon

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Voilà la réponse du serveur !");
+});
+
+server.listen(process.env.PORT || 3000);
+console.log("Hello");
+console.log("mabo");
