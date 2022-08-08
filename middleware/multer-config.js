1d5d6180f04af9e25560b30f56e5f800c7d5fb8c
ABastinou <101596380+BASTINOU777@users.noreply.github.com>
2022-08-08 18:39:12 +0200

    ajout de multer config.js

---
+++ middleware/multer-config.js
@@ -0,0 +1,27 @@
+//importation du package multer
+const multer = require("multer");
+//appel de MINE TYPE pour les images
+const MIME_TYPES = {
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
+};
+//fonction "diskStorage" pour enregistrer sur le disque
+const storage = multer.diskStorage({
+  //fonction "destination"qui prend 3 arguments (requète, file et un callback)
+  destination: (req, file, callback) => {
+    //appel du callback avec le dossier "images"
+    callback(null, "images");
+  },
+  //fonction "filename" qui prends 3 arguments
+  filename: (req, file, callback) => {
+    // fonction d'origine du nom avec la méthode split et appellant .join
+    const name = file.originalname.split(" ").join("_");
+    //utilisation de MINE TYPES
+    const extension = MIME_TYPES[file.mimetype];
+    //appel du calback en ajoutant un date now
+    callback(null, name + Date.now() + "." + extension);
+  },
+});
+//appel de la méthode multer et on lui passe storage
+module.exports = multer({ storage: storage }).single("image"); //
