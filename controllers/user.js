b6a5779b34591035510a7f8551554e963015fc60
ABastinou <101596380+BASTINOU777@users.noreply.github.com>
2022-08-08 15:24:54 +0200

    construction page user/controller

--- controllers/user.js
+++ controllers/user.js
@@ -29,25 +29,39 @@ module.exports.signup = (req, res, next) => {
 
 // Fonction Midleware "login" pour la connexion des utilisateurs existants
 module.exports.login = (req, res, next) => {
+  //méthode findOne qui permet de vérifier si l'user existe et que c'est bien son mot de passe
   User.findOne({ email: req.body.email })
+    //vérification que l'utilisateur à était trouvé
     .then((user) => {
+      //si utilisateur est null
       if (!user) {
-        return res.status(401).json({ error: "Utilisateur non trouvé !" });
+        //retourne une erreur avec un message flou
+        return res
+          .status(401)
+          .json({ error: "Paire login/mot de pass incorecte !" });
       }
+      //fonction bcrypt
       bcrypt
+        //méthode "compae" de bcrypt pour comparer le mtp
         .compare(req.body.password, user.password)
         .then((valid) => {
+          //si mot de passe correct
           if (!valid) {
+            //sinon retourne une erreur 401 avec message flou
             return res.status(401).json({ error: "Mot de passe incorrect !" });
           }
+          //si le mot de pass correct retourne code 200 avec un ojbjet avec token et user id
           res.status(200).json({
             userId: user._id,
             token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+              //expire en 24h
               expiresIn: "24h",
             }),
           });
         })
+        //sinon erreur server (500)
         .catch((error) => res.status(500).json({ error }));
     })
+    //sinon erreur de donnés 501
     .catch((error) => res.status(501).json({ error }));
 };
